5965e0c2471e04a7d121d42b16dfe9b3abdaf401
Cristian Orozco <cristianorozco1984@gmail.com>
2019-12-18 11:02:01 -0500

    carousel

--- dist/js/script.js
+++ dist/js/script.js
@@ -5,3 +5,14 @@ const $nav = document.querySelector('.nav')
 $burgerIcon.addEventListener('click', () => {
   $nav.classList.toggle('toggle-button')
 })
+
+
+// Initialize Owl Carousel
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    autoplay: false,
+    autoplayTimeout: 3000,
+    dots: false,
+    nav: true,
+    navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
+  });
